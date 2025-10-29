e98b33f370a2a361bf92b8ef3b4a7d853afbb271
Prajwalais <prajwalbakaningi7@gmail.com>
2025-10-30 02:12:55 +0530

    Create evenodd.py

---
+++ evenodd.py
@@ -0,0 +1,14 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+even_count = 0
+odd_count = 0
+
+# Count even and odd numbers
+for num in numbers:
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+
+print(f"Even numbers: {even_count}")
+print(f"Odd numbers: {odd_count}")
